3a1fbe26aecfe8b888937155f501ec9f61075042
Tobias Faust <tobias.faust@gmx.net>
2025-02-20 12:10:04 +0100

    feat: migrate FlowerCare to use NimBLE for improved BLE functionality

--- src/flowercare.cpp
+++ src/flowercare.cpp
@@ -2,24 +2,27 @@
 #include "baseconfig.h"
 
 FlowerCare::FlowerCare() : previousMillis(0), previousBatteryMillis(0) {
-    BLEDevice::init("");
-    pBLEScan = BLEDevice::getScan();
+    NimBLEDevice::init("");
+    pBLEScan = NimBLEDevice::getScan();
     pBLEScan->setActiveScan(true);
     ScanBLE();
 }
 
 void FlowerCare::ScanBLE() {
-    BLEScanResults foundDevices = pBLEScan->start(5, false);
-    for (int i = 0; i < foundDevices.getCount(); ++i) {
-        BLEAdvertisedDevice device = foundDevices.getDevice(i);
-        if (device.haveServiceUUID() && device.getServiceUUID().equals(BLEUUID("0000fe95-0000-1000-8000-00805f9b34fb"))) {
-            addDevice(device.getAddress().toString());
+    bool scanStarted = pBLEScan->start(5, false);
+    if (scanStarted) {
+        NimBLEScanResults foundDevices = pBLEScan->getResults();
+        for (int i = 0; i < foundDevices.getCount(); ++i) {
+            const NimBLEAdvertisedDevice* device = foundDevices.getDevice(i);
+            if (device->haveServiceUUID() && device->getServiceUUID().equals(NimBLEUUID("0000fe95-0000-1000-8000-00805f9b34fb"))) {
+                addDevice(device->getAddress());
+            }
         }
+        pBLEScan->clearResults();
     }
-    pBLEScan->clearResults();
 }
 
-void FlowerCare::addDevice(std::string address) {
+void FlowerCare::addDevice(NimBLEAddress address) {
     for (auto& device : devices) {
         if (device.address == address) {
             return;
@@ -31,52 +34,54 @@ void FlowerCare::addDevice(std::string address) {
 void FlowerCare::ReadSensors() {
     for (auto& device : devices) {
         updateDeviceData(device);
+        updateBatteryLevel(device);
     }
 }
 
 void FlowerCare::updateDeviceData(FlowerCareDevice& device) {
-    BLEClient* pClient = BLEDevice::createClient();
-    if (pClient->connect(BLEAddress(device.address.c_str()))) {
-        BLERemoteService* pRemoteService = pClient->getService(BLEUUID("00001204-0000-1000-8000-00805f9b34fb"));
+    NimBLEClient* pClient = NimBLEDevice::createClient();
+    if (pClient->connect(device.address)) {
+        NimBLERemoteService* pRemoteService = pClient->getService(NimBLEUUID("00001204-0000-1000-8000-00805f9b34fb"));
         if (pRemoteService) {
             // Send real-time data read request
-            BLERemoteCharacteristic* pWriteCharacteristic = pRemoteService->getCharacteristic(BLEUUID("00001a00-0000-1000-8000-00805f9b34fb"));
+            NimBLERemoteCharacteristic* pWriteCharacteristic = pRemoteService->getCharacteristic(NimBLEUUID("00001a00-0000-1000-8000-00805f9b34fb"));
             if (pWriteCharacteristic) {
                 uint8_t requestData[2] = {0xA0, 0x1F};
                 pWriteCharacteristic->writeValue(requestData, 2, true);
             }
 
             // Read sensor data
-            BLERemoteCharacteristic* pReadCharacteristic = pRemoteService->getCharacteristic(BLEUUID("00001a01-0000-1000-8000-00805f9b34fb"));
+            NimBLERemoteCharacteristic* pReadCharacteristic = pRemoteService->getCharacteristic(NimBLEUUID("00001a01-0000-1000-8000-00805f9b34fb"));
             if (pReadCharacteristic) {
-                std::string value = pReadCharacteristic->readValue();
+                String value = pReadCharacteristic->readValue().c_str();
                 device.temperature = (value[0] | (value[1] << 8)) / 10.0;
                 device.brightness = value[3] | (value[4] << 8) | (value[5] << 16) | (value[6] << 24);
                 device.moisture = value[7];
                 device.fertility = value[8] | (value[9] << 8);
             }
         }
-        pClient->disconnect();
     }
-    delete pClient;
+    NimBLEDevice::deleteClient(pClient);
 }
 
 void FlowerCare::updateBatteryLevel(FlowerCareDevice& device) {
-    BLEClient* pClient = BLEDevice::createClient();
-    if (pClient->connect(BLEAddress(device.address.c_str()))) {
-        BLERemoteService* pRemoteService = pClient->getService(BLEUUID("00001204-0000-1000-8000-00805f9b34fb"));
+    NimBLEClient* pClient = NimBLEDevice::createClient();
+    if (pClient->connect(device.address)) {
+        NimBLERemoteService* pRemoteService = pClient->getService(NimBLEUUID("00001204-0000-1000-8000-00805f9b34fb"));
         if (pRemoteService) {
             // Read battery level and firmware version
-            BLERemoteCharacteristic* pBatteryCharacteristic = pRemoteService->getCharacteristic(BLEUUID("00001a02-0000-1000-8000-00805f9b34fb"));
+            NimBLERemoteCharacteristic* pBatteryCharacteristic = pRemoteService->getCharacteristic(NimBLEUUID("00001a02-0000-1000-8000-00805f9b34fb"));
             if (pBatteryCharacteristic) {
-                std::string batteryValue = pBatteryCharacteristic->readValue();
-                device.battery = batteryValue[0];
-                device.firmwareVersion = batteryValue.substr(2, 4);
+                String batteryValue = pBatteryCharacteristic->readValue().c_str();
+                if (batteryValue.length() >= 4) {
+                    // Convert hex string to battery level and firmware version
+                    device.battery = static_cast<int>(strtol(batteryValue.substring(0, 2).c_str(), nullptr, 16));
+                    device.firmwareVersion = String(static_cast<char>(strtol(batteryValue.substring(2, 4).c_str(), nullptr, 16)));
+                }
             }
         }
-        pClient->disconnect();
     }
-    delete pClient;
+    NimBLEDevice::deleteClient(pClient);
 }
 
 void FlowerCare::loop() {
--- src/flowercare.h
+++ src/flowercare.h
@@ -2,22 +2,23 @@
 #define FLOWERCARE_H
 
 #include <vector>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEScan.h>
-#include <BLEAdvertisedDevice.h>
+#include <NimBLEDevice.h>
+#include <NimBLEUtils.h>
+#include <NimBLEScan.h>
+#include <NimBLEAdvertisedDevice.h>
+#include <Arduino.h>
 
 class FlowerCareDevice {
 public:
-    std::string address;
+    NimBLEAddress address;
     int battery;
     int brightness;
     float temperature;
     int moisture;
     int fertility;
-    std::string firmwareVersion;
+    String firmwareVersion;
 
-    FlowerCareDevice(std::string addr) : address(addr), battery(0), brightness(0), temperature(0.0), moisture(0), fertility(0), firmwareVersion("") {}
+    FlowerCareDevice(NimBLEAddress addr) : address(addr), battery(0), brightness(0), temperature(0.0), moisture(0), fertility(0), firmwareVersion("") {}
 };
 
 class FlowerCare {
@@ -28,13 +29,13 @@ public:
     void loop();
 
 private:
-    BLEScan* pBLEScan;
+    NimBLEScan* pBLEScan;
     std::vector<FlowerCareDevice> devices;
     unsigned long previousMillis;
     unsigned long previousBatteryMillis;
     const unsigned long interval = 5 * 60 * 1000; // 5 minutes
 
-    void addDevice(std::string address);
+    void addDevice(NimBLEAddress address);
     void updateDeviceData(FlowerCareDevice& device);
     void updateBatteryLevel(FlowerCareDevice& device);
 };
