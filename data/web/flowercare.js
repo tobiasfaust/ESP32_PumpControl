21941990731ead690a3193ce32eb1bca66715955
Tobias Faust <tobias.faust@gmx.net>
2025-03-02 12:14:38 +0100

    test

--- data/web/flowercare.js
+++ data/web/flowercare.js
@@ -2,7 +2,101 @@ import * as global from './Javascript.js';
 import * as valveFn from './valvefunctions.js';
 
 // ************************************************
+
 export function init() {
+  var json = {
+    "cmd": {
+        "action": "GetInitData",
+        "subaction": "flowercare",
+        "callbackFn": "flowercare_Callback"
+    },
+    "data": {
+        "flowercare": [
+            {
+                "address": "c4:7c:8d:62:8e:04",
+                "mqtttopic": "flowercare/c4:7c:8d:62:8e:04",
+                "battery": 49,
+                "firmwareVersion": "2.7.0",
+                "temperature": 17.8,
+                "moisture": 0,
+                "brightness": 92,
+                "fertility": 0,
+                "lastLiveDataUpdate": 347962,
+                "lastBatteryUpdate": 346362,
+                "failedReads": 0,
+                "active": {
+                    "checked": true,
+                    "data-mac": "c4:7c:8d:62:8e:04"
+                }
+            },
+            {
+                "address": "c4:7c:8d:62:8e:a2",
+                "mqtttopic": "flowercare/c4:7c:8d:62:8e:a2",
+                "battery": 50,
+                "firmwareVersion": "2.7.0",
+                "temperature": 17.4,
+                "moisture": 0,
+                "brightness": 113,
+                "fertility": 0,
+                "lastLiveDataUpdate": 353062,
+                "lastBatteryUpdate": 351512,
+                "failedReads": 0,
+                "active": {
+                    "checked": true,
+                    "data-mac": "c4:7c:8d:62:8e:a2"
+                }
+            },
+            {
+                "address": "c4:7c:8d:62:8e:76",
+                "mqtttopic": "flowercare/c4:7c:8d:62:8e:76",
+                "battery": 60,
+                "firmwareVersion": "2.7.0",
+                "temperature": 17,
+                "moisture": 1,
+                "brightness": 110,
+                "fertility": 0,
+                "lastLiveDataUpdate": 356291,
+                "lastBatteryUpdate": 51559,
+                "failedReads": 1,
+                "active": {
+                    "checked": true,
+                    "data-mac": "c4:7c:8d:62:8e:76"
+                }
+            }
+        ],
+        "fc_relations": [
+            {
+                "active": {
+                    "checked": false
+                },
+                "treshold": 30,
+                "duration": 60
+            },
+            {
+                "active": {
+                    "checked": false
+                },
+                "treshold": 30,
+                "duration": 60
+            }
+        ]
+    },
+    "js": {
+        "esp_uptime": 361485,
+        "gpio_disabled": "[221,222,0]",
+        "availablePorts": "[]",
+        "configuredPorts": "[{\"port\":216, \"name\":\"Valve1\"},{\"port\":217, \"name\":\"Valve2\"}]"
+    },
+    "response": {
+        "status": 1,
+        "text": "successful"
+    }
+}
+
+  MyCallback(json);
+}
+
+export function init1() {
   // Initiale Verbindung aufbauen
     global.connectWebSocket();
   
