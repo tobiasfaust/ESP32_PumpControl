d8f19a26fe1bbf74a32e365d32568ba8beb2688a
Tobias Faust <tobias.faust@gmx.net>
2025-08-02 06:54:38 +0200

    fix storing mqtt basepath, emtpy basepath now allowed

--- src/baseconfig.cpp
+++ src/baseconfig.cpp
@@ -4,7 +4,7 @@ BaseConfig::BaseConfig():
   mqtt_server ("test.mosquitto.org"),
   mqtt_port(1883),
   mqtt_root("PumpControl"),
-  mqtt_basepath("home/"),
+  mqtt_basepath("home"),
   mqtt_UseRandomClientID(true),
   keepalive(0),
   debuglevel(3),
@@ -61,7 +61,7 @@ void BaseConfig::LoadJsonConfig() {
           if (elem["mqttport"])         { this->mqtt_port = elem["mqttport"].as<int>();}
           if (elem["mqttuser"])         { this->mqtt_username = elem["mqttuser"].as<String>();}
           if (elem["mqttpass"])         { this->mqtt_password = elem["mqttpass"].as<String>();}
-          if (elem["mqttbasepath"])     { this->mqtt_basepath = elem["mqttbasepath"].as<String>();}
+          if (elem["mqttbasepath"])     { this->mqtt_basepath = elem["mqttbasepath"].as<String>();} else { this->mqtt_basepath = ""; }
           if (elem["UseRandomClientID"]){ if (elem["UseRandomClientID"].as<String>() == "none") { this->mqtt_UseRandomClientID=false;} else {this->mqtt_UseRandomClientID=true;}}
           if (elem["keepalive"])        { if (elem["keepalive"].as<int>() == 0) { this->keepalive = 0;} else { this->keepalive = _max(elem["keepalive"].as<int>(), 10);}}
           if (elem["debuglevel"])       { this->debuglevel = _max(elem["debuglevel"].as<int>(), 0);}
@@ -85,7 +85,7 @@ void BaseConfig::LoadJsonConfig() {
     this->logN(1, "baseconfig.json config File not exists, load default BaseConfig");
   }
 
-  // Data Cleaning
+  // Data Cleaning, needed without trailing slash
   if(this->mqtt_basepath.endsWith("/")) {
     this->mqtt_basepath = this->mqtt_basepath.substring(0, this->mqtt_basepath.length()-1); 
   }
